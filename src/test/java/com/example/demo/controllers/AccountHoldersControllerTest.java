3503a4565aaf9a117584221dfd3afcb95c8e2689
xavi-dig <xromero82@gmail.com>
2022-12-11 23:45:35 +0100

    Test in postman ok

--- src/test/java/com/example/demo/controllers/AccountHoldersControllerTest.java
+++ src/test/java/com/example/demo/controllers/AccountHoldersControllerTest.java
@@ -1,2 +1,78 @@
-package com.example.demo.controllers;public class AccountHoldersControllerTest {
+package com.example.demo.controllers;
+
+import com.example.demo.models.users.AccountHolders;
+import com.example.demo.models.users.Address;
+import com.example.demo.repositories.account.AccountRepository;
+import com.example.demo.repositories.account.CheckingRepository;
+import com.example.demo.repositories.user.AccountHoldersRepository;
+import com.example.demo.repositories.user.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class AccountHoldersControllerTest {
+/*
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private AccountHoldersRepository accountHoldersRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CheckingRepository checkingRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = JsonMapper.builder()
+            .addModule(new JavaTimeModule())
+            .build();
+
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    void commandLineRunnerRuns() {
+        assertEquals(4, userRepository.findAll().size());
+    }
+
+    @Test
+    void addNewAccountHolder_OK() throws Exception {
+        AccountHolders accountHolder = new AccountHolders("Juan",
+                LocalDate.of(1994, 11, 28), new Address("Froilan de Todos los Santos",
+                1111, "Barcelona", "España"), "elfroi@example.com","123");
+
+        String body = objectMapper.writeValueAsString(accountHolder);
+
+        MvcResult result = mockMvc.perform(post("/accounts").content(body).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+
+    }*/
 }
+
+
--- src/test/java/com/example/demo/controllers/ThirdPartyControllerTest.java
+++ src/test/java/com/example/demo/controllers/ThirdPartyControllerTest.java
@@ -1,2 +1,81 @@
-package com.example.demo.controllers;public class ThirdPartyControllerTest {
-}
+package com.example.demo.controllers;
+
+import com.example.demo.models.users.Admin;
+import com.example.demo.models.users.ThirdParty;
+import com.example.demo.repositories.account.RoleRepository;
+import com.example.demo.repositories.user.ThirdPartyRepository;
+import com.example.demo.repositories.user.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class ThirdPartyControllerTest {
+/*
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    ThirdPartyRepository thirdPartyRepository;
+
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        roleRepository.deleteAll();
+        userRepository.deleteAll();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        objectMapper.findAndRegisterModules();
+    }
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    //La etiqueta de abajo tan solo sirve para pasarle el username y password para el userDetails pero la securizacion de la ruta no se aplica
+    //en los tests para que lo recordeis
+    @WithMockUser(username = "user1", password = "pwd")
+    void createUser() throws Exception {
+        String body = objectMapper.writeValueAsString(new Admin("Admin","123"));
+        System.out.println(body);
+        MvcResult mvcResult = mockMvc.perform(post("/user/").content(body).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+    }
+
+    @Test
+    @WithMockUser(username = "admin", password = "1234")
+    void createThirdPartyUser_works() throws Exception {
+        ThirdParty thirdParty = new ThirdParty("Xavi", "1234");
+        String body = objectMapper.writeValueAsString(thirdParty);
+        System.out.println(body);
+
+        MvcResult mvcResult = mockMvc.perform(post("/create-third-party-user").
+                content(body).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andReturn();
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("Xavi"));
+    }
+
+*/}
